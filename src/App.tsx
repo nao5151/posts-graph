5e42a994ffda4b2da643443e6d157844b3be331d
Naoyuki Miyazaki <miyazaki@buddying.jp>
2019-01-16 11:30:25 +0900

    Style

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
+  state = {
+    vertical: false,
+    months: [
+      'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+    ],
+    weeks: [1, 2, 3, 4, 5]
+  }
+
   render() {
+    const { vertical, months, weeks } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <button onClick={() => this.setState({vertical: !vertical})}>change view</button>
+        <div
+          className={`graph ${vertical ? 'graph--vertical' : ''}`}
+        >
+          <div className="graph__months">
+            {months.map(m => (
+              <div className="graph__month">{m}</div>
+            ))}
+          </div>
+          <div className="graph__weeks">
+            {weeks.map(w => (
+              <div className="graph__week">{w}</div>
+            ))}
+          </div>
+          <div className="graph__posts">
+            {Array.from({length: 12}).map((_, i) => (
+              <>
+              {weeks.map((w, i) => (
+                <div className={`graph__post graph__post--${i}`}>
+                  <span className="graph__post-label">1月{w}週目 {i}記事</span>
+                </div>
+              ))}
+              </>
+            ))}
+          </div>
+        </div>
+      </>
     );
   }
 }
