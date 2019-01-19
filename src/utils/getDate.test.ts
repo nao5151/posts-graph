101d3eb04e2ba0a7a3c91616fb249cd56ff78d29
Naoyuki Miyazaki <miyazaki@buddying.jp>
2019-01-19 10:23:35 +0900

    update test

--- src/utils/getDate.test.ts
+++ src/utils/getDate.test.ts
@@ -1,23 +1,119 @@
 import moment from 'moment'
 import getDate from './getDate'
 
+moment.locale('ja')
+
 it('last year', () => {
-  let date = moment('2018-12-30T00:00:00').utcOffset("+09:00").toISOString()
+  let date = moment([2018, 11, 30]).toISOString()
   expect(getDate(date)).toMatchObject({year: 2018, month: 11, week: 4})
 });
 
 it('week 1', () => {
-  let date = moment('2018-12-31T00:00:00').utcOffset("+09:00").toISOString()
+  let date = moment([2018, 11, 31]).toISOString()
   expect(getDate(date)).toMatchObject({year: 2019, month: 0, week: 0})
-  date = moment('2019-01-01T00:00:00').utcOffset("+09:00").toISOString()
-  expect(getDate(date)).toMatchObject({year: 2019, month: 0, week: 0})
-  date = moment('2019-01-06T23:59:59').utcOffset("+09:00").toISOString()
+  date = moment([2019, 0, 6]).toISOString()
   expect(getDate(date)).toMatchObject({year: 2019, month: 0, week: 0})
 });
 
 it('week 2', () => {
-  let date = moment('2019-01-07T00:00:00').utcOffset("+09:00").toISOString()
+  let date = moment([2019, 0, 7]).toISOString()
   expect(getDate(date)).toMatchObject({year: 2019, month: 0, week: 1})
-  date = moment('2019-01-13T23:59:59').utcOffset("+09:00").toISOString()
+  date = moment([2019, 0, 13]).toISOString()
   expect(getDate(date)).toMatchObject({year: 2019, month: 0, week: 1})
 });
+
+it('1/1 is first week', () => {
+  for(let i = 2000; i < 3000; i++) {
+      expect(
+          getDate(moment([i, 0, 1]))
+      ).toMatchObject({year: i, month: 0, week: 0})
+  }
+})
+
+it('2/1 is first week of month', () => {
+  for(let i = 2000; i < 3000; i++) {
+      expect(
+          getDate(moment([i, 1, 1]))
+      ).toMatchObject({year: i, month: 1, week: 0})
+  }
+})
+
+it('3/1 is first week of month', () => {
+  for(let i = 2000; i < 3000; i++) {
+      expect(
+          getDate(moment([i, 2, 1]))
+      ).toMatchObject({year: i, month: 2, week: 0})
+  }
+})
+
+it('4/1 is first week of month', () => {
+  for(let i = 2000; i < 3000; i++) {
+      expect(
+          getDate(moment([i, 3, 1]))
+      ).toMatchObject({year: i, month: 3, week: 0})
+  }
+})
+
+it('5/1 is first week of month', () => {
+  for(let i = 2000; i < 3000; i++) {
+      expect(
+          getDate(moment([i, 4, 1]))
+      ).toMatchObject({year: i, month: 4, week: 0})
+  }
+})
+
+it('6/1 is first week of month', () => {
+  for(let i = 2000; i < 3000; i++) {
+      expect(
+          getDate(moment([i, 5, 1]))
+      ).toMatchObject({year: i, month: 5, week: 0})
+  }
+})
+
+it('7/1 is first week of month', () => {
+  for(let i = 2000; i < 3000; i++) {
+      expect(
+          getDate(moment([i, 6, 1]))
+      ).toMatchObject({year: i, month: 6, week: 0})
+  }
+})
+
+it('8/1 is first week of month', () => {
+  for(let i = 2000; i < 3000; i++) {
+      expect(
+          getDate(moment([i, 7, 1]))
+      ).toMatchObject({year: i, month: 7, week: 0})
+  }
+})
+
+it('9/1 is first week of month', () => {
+  for(let i = 2000; i < 3000; i++) {
+      expect(
+          getDate(moment([i, 8, 1]))
+      ).toMatchObject({year: i, month: 8, week: 0})
+  }
+})
+
+it('10/1 is first week of month', () => {
+  for(let i = 2000; i < 3000; i++) {
+      expect(
+          getDate(moment([i, 9, 1]))
+      ).toMatchObject({year: i, month: 9, week: 0})
+  }
+})
+
+it('11/1 is first week of month', () => {
+  for(let i = 2000; i < 3000; i++) {
+      expect(
+          getDate(moment([i, 10, 1]))
+      ).toMatchObject({year: i, month: 10, week: 0})
+  }
+})
+
+it('12/1 is first week of month', () => {
+  for(let i = 2000; i < 3000; i++) {
+      expect(
+          getDate(moment([i, 11, 1]))
+      ).toMatchObject({year: i, month: 11, week: 0})
+  }
+})
--- src/utils/getDate.ts
+++ src/utils/getDate.ts
@@ -2,11 +2,11 @@ import moment from 'moment'
 moment.updateLocale('ja', {
   week : {
     dow : 1,
-    doy : 1,
+    doy : 7,
   },
 })
 
-export default function getDate(dateValue: string | number | Date) {
+export default function getDate(dateValue: string | number | Date | moment.Moment) {
   const date = moment(dateValue)
   const lastDayOfWeek = date.endOf('week')
   const firstDayOfMonth = lastDayOfWeek.clone().startOf('month')
